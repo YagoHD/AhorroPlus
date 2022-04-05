f2ccb70289287ba19b795b5851806f5d408a2634
Yago Ramos Bermúdez <yagorb@hotmail.com>
2022-04-05 12:45:24 +0200

    Entrega #1 ADD: Botones Mi Lista y Antiguas en el main activity, mi lista es funcional y conecta con MyListActivity. ADD: Nuevo activity MyListActivity. ADD: 3 nuevos iconos (Mercadona, Eroski y Carrefour) ADD: Creado y subido archivo .json con los productos y precios en respectivos establecimientos.

--- app/src/main/java/com/example/ahorroplus/MainActivity.java
+++ app/src/main/java/com/example/ahorroplus/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ahorroplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonMiLista = findViewById(R.id.buttonMiLista);
+            buttonMiLista.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), MyListActivity.class);
+                    startActivityForResult(intent, 0);
+                }
+            });
+
+
+        
+
     }
 }
